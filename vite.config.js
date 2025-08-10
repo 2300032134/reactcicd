f6da2619bc275371d7748b33244a7d7326a91829
k madhuri <163718247+2300032134@users.noreply.github.com>
2025-08-10 15:27:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reactcicd/', // important for GitHub Pages
+  base: '/reactcicd/', // 👈 must match your repo name exactly
 })
